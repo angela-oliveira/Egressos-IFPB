cf3f01b3b42ff5d74eded18db5a6e77ef4018eec
Fabrício Liberato <fabricio.liber@gmail.com>
2019-08-10 12:13:57 -0300

    Configura webpack e modulos js

---
+++ js/utils.mjs
@@ -0,0 +1,15 @@
+export const CAMPUS = {
+    'ifpb-jp': 'João Pessoa',
+    'ifpb-cz': 'Cajazeiras',
+    'ifpb-cg': 'Campina Grande',
+    'ifpb-cb': 'Cabedelo'
+}
+
+export const CURSOS = {
+    'cstsi': 'Sistemas para Internet',
+    'cstrc': 'Redes de Computadores',
+    'Engenharia Eletrica': 'Engenharia Eletrica',
+    'CST EM TELECOMUNICAÇÕES': 'Telecomunicações',
+    'ads': 'Análize e Desenhvolvimento de Sistemas',
+    'cstt': 'Telecomunicações'
+}
